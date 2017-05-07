28c599f3ca96ecddf2ccc39eebd3d73262bfbf00
richardpassos01 <richard.h.passos@hotmail.com>
2017-05-07 01:48:43 -0300

    Update search.php

--- plugin/php/search.php
+++ plugin/php/search.php
@@ -18,19 +18,30 @@
 		$consulta = 'select cod_mercadoria as "id", tipo_mercadoria as "tipo",nome_mercadoria as "nome",quantidade as "qtd", 
 		preco as "preco", tipo_negocio as "negocio" from plataforma_mercadoria where cod_mercadoria='. $_POST['busca'].'';	
 
-		$resultado = OCIParse($conn, $consulta);		
-		 oci_execute($resultado);	
+		$resultado = OCIParse($conn, $consulta);	//oci_parse equivale ao mysql_query()	
+		oci_execute($resultado);	
 
-			while($row = oci_fetch_assoc($resultado)): //Consulta e  imprimi query
-				echo	
+		$row = oci_fetch_assoc($resultado);	//oci_fetch_assoc() equivale ao mysql_fetch_assoc()
+
+			if($row>0){									
+				echo		//Imprimi os dados do produto se o ID for valido
 				"<h4><ul><strong>ID de Protocolo gerado: </strong>" 			. $row['id'] . 
 				"<br/><br/><strong>Tipo de Mercadoria: </strong>"   			. $row['tipo'] . 
 				"<br/><br/><strong>Nome do produto: </strong>" 				. $row['nome'] . 
 				"<br/><br/><strong>Quantidade de produtos: </strong>" 			. $row['qtd'] . 
-				"<br/><br/><strong>O valor do produto anunciado em reis é: </strong>" 	. $row['preco'] . 
+				"<br/><br/><strong>O valor do produto anunciado em reis é: </strong>"	. $row['preco'] . 
 				"<br/><br/><strong>Você optou por fazer uma: </strong>" 		. $row['negocio'] . 
 				"</br>";
-			endwhile;					
+			}
+
+				else{ 		//Imprimi alert abaixo se o ID for invalido
+				echo'<script type="text/javascript"> alert("Produto não encontrado")</script>';
+					
+				echo"<strong>Não encontramos em nosso banco o produto de ID: ". $_POST['busca']."</strong>
+				<br/><h6>Por gentileza verificar se o numero de protocolo gerado está correto.<br/></br>
+				Em casos de duvida, entre em contato com a equipe de suporte via 
+				<a href='https://br.linkedin.com/in/richard-henrique-451a8979'>Linkedin</a></h6>";	
+			}					
 		?>				
 	</body>	
 </html>
