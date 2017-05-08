4eb88fc35d18598f685507b9f76b08b9893a7fa7
richardpassos01 <richard.h.passos@hotmail.com>
2017-05-08 01:41:23 -0300

    Add files via upload

---
+++ plugin/js/validacao.js
@@ -0,0 +1,34 @@
+function confirma(){
+alert("Produto Cadastrado com sucesso!");
+}
+
+
+function validar() {
+
+if(document.FormularioLogin.tipo_mercadoria.value==""){
+	alert('Por favor informar o tipo de mercadoria');
+	FormularioLogin.tipo_mercadoria.focus();
+	return false;
+}
+
+if(document.FormularioLogin.nome_mercadoria.value==""){
+	alert('Por favor informar o nome da mercadoria');
+	FormularioLogin.nome_mercadoria.focus();
+	return false;
+}
+
+if(document.FormularioLogin.quantidade.value.length < 1){
+	alert('A quantidade não pode ser menor do que 1');
+		FormularioLogin.quantidade.focus();
+	return false;
+}
+
+if(document.FormularioLogin.preco.value.length < 1){
+	alert('O preço não pode ser 0');
+		FormularioLogin.preco.focus();
+	return false;
+}
+
+
+
+}
